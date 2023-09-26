d9c456aa32c01c50d4ead3648e4eb03e47f5236d
Sebastián Bedoya <sebastianbedoya25@gmail.com>
2023-09-25 21:11:23 -0500

    Creación proyecto

---
+++ components/Button.tsx
@@ -0,0 +1,5 @@
+const Button = () => {
+    return <button>Hola</button>;
+}
+
+export default Button;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Button from "@/components/Button";
+
+const paginaPrincipal = () => {
+  return <div>
+    <span className="text-cyan-500 bg-slate-800">
+        Hola desde mi cv
+      </span>
+    {/* <div><Button /></div> */}
+    </div>
+}
+export default paginaPrincipal;
